b93de96f02ef2af40eb6febee57025ccd601e52d
amjoshi <amjoshi@twilio.com>
2022-12-15 15:21:23 +0200

    analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { inject } from '@vercel/analytics';
 
 import './index.css';
 import './custom.scss';
@@ -12,6 +13,8 @@ root.render(
     <App />
   </React.StrictMode>,
 );
+
+inject();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
